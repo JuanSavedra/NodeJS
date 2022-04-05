5935422aa218dd86a7d96e6a99fa1e9bf19969ad
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-04-05 15:24:05 -0300

    http methods

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+/* Chamadas HTTP
+GET - Busca informações dentro do servidor
+POST - Insere informações dentro do servidor
+PUT - Altera informações dentro do servidor
+PATCH - Altera informações específicas dentro do servidor
+DELETE - Deleta informações dentro do servidor
+*/
+
 const express = require('express');
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.send("Hello World!")
+app.get("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
+
+app.post("/courses", (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(["Curso 5", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["Curso 5", "Curso 6", "Curso 3", "Curso 4"]);
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Curso 5", "Curso 2", "Curso 4"]);
+})
 
 app.listen(3333);
